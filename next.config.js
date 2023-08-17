bfeb3ab293003411106830639732084b46277b66
sumandul <dulalsuman853@gmail.com>
2023-08-17 21:53:10 +0545

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,1 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-});
+
